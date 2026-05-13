8f3281204bf411c1abbc942d66c2481691f4aa77
Chahal <balrajsingh832@gmail.com>
2026-05-13 14:09:27 +0530

    fix port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const os = require('os');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
 
 app.use(cors({ origin: '*' }));
 app.use(express.json());
